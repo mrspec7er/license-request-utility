733137d4d8225b440486bf8e2719e20f5a3fb2e1
Kusuma Sandi <wijayakusumasandi>
2024-04-27 15:52:51 +0800

    feat: 🎸 update migration script

--- script/database/main.go
+++ script/database/main.go
@@ -28,7 +28,7 @@ func CreateDB() *gorm.DB {
 		panic(err)
 	}
 
-	cmd := fmt.Sprintf("SELECT 'CREATE DATABASE %s' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '%s')", os.Getenv("DB_NAME"), os.Getenv("DB_NAME"))
+	cmd := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", os.Getenv("DB_NAME"))
 	db.Exec(cmd)
 
 	fmt.Println("Database created!")
