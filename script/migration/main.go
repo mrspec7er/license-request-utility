df20e98786718ed0e0e93578b50dfbe1e75af812
Kusuma Sandi <wijayakusumasandi>
2024-05-01 09:45:25 +0800

    fix: 🐛 remove id from migration

--- script/migration/main.go
+++ script/migration/main.go
@@ -31,28 +31,23 @@ func SeedDatabase(DB *gorm.DB) {
 	if formCount == 0 {
 		DB.Create([]*dto.Form{
 			{
-				BaseModel: dto.BaseModel{ID: 1},
-				Name:      "Driving License",
-				Category:  "General",
+				Name:     "Driving License",
+				Category: "General",
 				Sections: []*dto.Section{
 					{
-						BaseModel: dto.BaseModel{ID: 1},
-						Name:      "Biodata",
+						Name: "Biodata",
 						Fields: []*dto.Field{
 							{
-								BaseModel:  dto.BaseModel{ID: 1},
 								Label:      "Name",
 								Type:       "String",
 								FieldOrder: 1,
 							},
 							{
-								BaseModel:  dto.BaseModel{ID: 2},
 								Label:      "Birth Date",
 								Type:       "Date",
 								FieldOrder: 2,
 							},
 							{
-								BaseModel:  dto.BaseModel{ID: 3},
 								Label:      "Gender",
 								Type:       "Option",
 								FieldOrder: 3,
@@ -60,23 +55,19 @@ func SeedDatabase(DB *gorm.DB) {
 						},
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 2},
-						Name:      "General Knowledge",
+						Name: "General Knowledge",
 						Fields: []*dto.Field{
 							{
-								BaseModel:  dto.BaseModel{ID: 4},
 								Label:      "Technical Skill (1-10)",
 								Type:       "Number",
 								FieldOrder: 1,
 							},
 							{
-								BaseModel:  dto.BaseModel{ID: 5},
 								Label:      "Practical Experience (1-10)",
 								Type:       "Number",
 								FieldOrder: 2,
 							},
 							{
-								BaseModel:  dto.BaseModel{ID: 6},
 								Label:      "Note",
 								Type:       "Text",
 								FieldOrder: 3,
@@ -121,34 +112,28 @@ func SeedDatabase(DB *gorm.DB) {
 				FormID: 1,
 				Responses: []*dto.Response{
 					{
-						BaseModel: dto.BaseModel{ID: 1},
-						FieldID:   1,
-						Value:     "Basic User",
+						FieldID: 1,
+						Value:   "Basic User",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 2},
-						FieldID:   2,
-						Value:     "2001-28-03",
+						FieldID: 2,
+						Value:   "2001-28-03",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 3},
-						FieldID:   3,
-						Value:     "Female",
+						FieldID: 3,
+						Value:   "Female",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 4},
-						FieldID:   4,
-						Value:     "8",
+						FieldID: 4,
+						Value:   "8",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 5},
-						FieldID:   5,
-						Value:     "7",
+						FieldID: 5,
+						Value:   "7",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 6},
-						FieldID:   6,
-						Value:     "Little bit too aggressive in corner but has very good handling and fast response",
+						FieldID: 6,
+						Value:   "Little bit too aggressive in corner but has very good handling and fast response",
 					},
 				},
 			},
@@ -158,19 +143,16 @@ func SeedDatabase(DB *gorm.DB) {
 				FormID: 1,
 				Responses: []*dto.Response{
 					{
-						BaseModel: dto.BaseModel{ID: 7},
-						FieldID:   1,
-						Value:     "Basic User",
+						FieldID: 1,
+						Value:   "Basic User",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 8},
-						FieldID:   3,
-						Value:     "Female",
+						FieldID: 3,
+						Value:   "Female",
 					},
 					{
-						BaseModel: dto.BaseModel{ID: 9},
-						FieldID:   4,
-						Value:     "8",
+						FieldID: 4,
+						Value:   "8",
 					},
 				},
 			},
