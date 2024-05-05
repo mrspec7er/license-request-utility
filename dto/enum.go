b43c88ab4ff2ed6b8a263d8f2d4ab518fb91508e
Kusuma Sandi <wijayakusumasandi>
2024-05-05 14:43:12 +0800

    feat: 🎸 added enum for resuest status and roles

--- dto/enum.go
+++ dto/enum.go
@@ -6,3 +6,14 @@ const (
 	UserContextKey key = "user"
 	AuthCookieName key = "auth"
 )
+
+const (
+	RequestNew      key = "NEW"
+	RequestRejected key = "REJECTED"
+	RequestApproved key = "APPROVED"
+)
+
+const (
+	RoleAdmin key = "ADMIN"
+	RoleUser  key = "USER"
+)
